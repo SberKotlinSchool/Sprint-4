0339f7fdb960096ccb14363cee99a48b69c81180
ADobrynin <adobrynin@t1-consulting.ru>
2021-09-18 09:55:49 +0300

    Добавил инициалиацию в StudentsGroup.kt Убрал equals из дата класса Student.kt

--- functional/src/main/kotlin/ru/sber/functional/Student.kt
+++ functional/src/main/kotlin/ru/sber/functional/Student.kt
@@ -3,25 +3,10 @@ package ru.sber.functional
 data class Student(
     val firstName: String,
     val lastName: String,
-    val middleName: String,
-    val age: Int,
+    val middleName: String = "No Middle Name",
+    val age: Int = 18,
     val averageRate: Double,
-    val city: String,
-    val specialization: String,
-    val prevEducation: String?
+    val city: String = "No City",
+    val specialization: String = "No Specialization",
+    val prevEducation: String? = null
 )
-{
-    override fun equals(other: Any?): Boolean {
-        if(other == null) return false
-        if(javaClass != other.javaClass) return false
-        other as Student
-        return (firstName == other.firstName
-                && lastName == other.lastName
-                && middleName == other.middleName
-                && age == other.age
-                && averageRate == other.averageRate
-                && city == other.city
-                && specialization == other.specialization
-                && prevEducation == other.prevEducation)
-    }
-}
