8f2a2d2031d19e87b8c5479c00dcd2c0935c5dee
Gaponchuk Alexey <aggaponchuk@gmail.com>
2022-09-29 12:33:14 +0300

    Exceptions tasks

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -7,12 +7,7 @@ object PowFactory {
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
 
-    infix fun Int.a(arg: Int): Double {
-        return this.toDouble().pow(arg)
-    }
-
     fun buildPowFunction(pow: Int): (Int) -> Int {
-
         return { param -> param.toDouble().pow(pow).toInt() }
     }
 }
