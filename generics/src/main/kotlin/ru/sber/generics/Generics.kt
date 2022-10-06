9266d800f4c07d908fe912cbbd33dafbd4d9722d
RVYakovlev <RVYakovlev@sberbank.ru>
2022-10-06 15:27:13 +0300

    Generics #1

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,40 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <A, B> compare(p1: Pair<A, B>, p2: Pair<A, B>): Boolean {
+    return p1.first == p2.first && p1.second == p2.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.count { it > elem }
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list
+            .apply{add(value)}
+            .sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack <T> {
+    private val data: ArrayDeque<T> = ArrayDeque()
+
+    fun isEmpty(): Boolean {
+        return data.isEmpty()
+    }
+
+    fun push(i: T) {
+        data.addFirst(i)
+    }
+
+    fun pop(): T {
+        return data.removeFirst()
+    }
 
 }
