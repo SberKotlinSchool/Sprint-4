5f4e1c2e5cebd1fdc755a7f68de4ed791bd89eaa
PavelPopov4Kotlin <palepopov@sberbank.ru>
2022-10-06 23:50:11 +0600

    generics resolved

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,34 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
+import kotlin.collections.ArrayList
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <A, B> compare(p1: Pair<A, B>, p2: Pair<A, B>): Boolean {
+    return p1.first==p2.first && p1.second==p2.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.count { member -> member.compareTo(elem) > 0 }
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = ArrayList()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    val innerStack = LinkedList<T>()
+    fun push(elem : T) = innerStack.push(elem)
 
+    fun pop() : T = innerStack.pop()
+
+    fun isEmpty() = innerStack.isEmpty()
 }
