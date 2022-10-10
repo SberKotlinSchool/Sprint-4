8dfafda77f60b77c7fc423db98549f87a1cd27fc
19069801 <aarozhok@sberbank.ru>
2022-10-10 14:53:47 +0300

    functional - правки согласно комментарию

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -5,15 +5,7 @@ import kotlin.random.Random
 class StudentsGroup {
     lateinit var students: MutableList<Student>
 
-    //понимаю, что predicate можно было просто передать в метод filter, думал, что надо реализовать что-то свое
-    fun filterByPredicate(predicate: (Student) -> Boolean): List<Student> {
-        var filteredList: MutableList<Student> = mutableListOf()
-         students.forEach {
-             if (predicate(it))
-                 filteredList.add(it)
-        }
-        return filteredList
-    }
+    fun filterByPredicate(predicate: (Student) -> Boolean) = students.filter(predicate)
 
     fun generateStudents(amount: Int) {
         this.students = mutableListOf()
@@ -39,29 +31,13 @@ class StudentsGroup {
             )
         )
     }
-
-//    fun printStudents() {
-//        println("firstName | lastName | middleName | averageRate")
-//        students.forEach {
-//            println("${it.firstName}| ${it.lastName} | ${it.middleName} | ${it.averageRate}")
-//        }
-//    }
 }
 
 fun main() {
     val studentsGroup = StudentsGroup()
-    studentsGroup.students = mutableListOf()
     studentsGroup.generateStudents(10)
 
-    studentsGroup.filterByPredicate { it.age != null }.forEach {
-        println("${it.firstName} | ${it.lastName} | ${it.middleName} | ${it.averageRate}")
-    }
+    studentsGroup.filterByPredicate { it.age == 24 }
 
-//    studentsGroup.filterByPredicate { it.averageRate > 3.0 }.forEach {
-//        println("${it.firstName}| ${it.lastName} | ${it.middleName} | ${it.averageRate}")
-//    }
-//
-//    studentsGroup.filterByPredicate { it.lastName.contains("2") }.forEach {
-//        println("${it.firstName}| ${it.lastName} | ${it.middleName} | ${it.averageRate}")
-//    }
+
 }
