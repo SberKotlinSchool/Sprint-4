a80dec25216520bfec6b875f3b538451e36ea5fb
Pavel <pavel@yandex.ru>
2022-10-12 14:12:05 +0300

    sprint-4 functional

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -4,7 +4,13 @@ object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(pow: Int): (Int) -> Int = { it pow pow }
+
+    private infix fun Int.pow(pow: Int): Int {
+        var result = 1
+        for (i in 1.. pow) {
+            result *= this
+        }
+        return result
     }
 }
