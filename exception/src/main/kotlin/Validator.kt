f20bbbe71e697453cf55d653e06d70a565c01fef
TheWeaklessOne <daniil.teterin.21@gmail.com>
2022-10-14 17:02:45 +0300

    Exception part done

--- exception/src/main/kotlin/Validator.kt
+++ exception/src/main/kotlin/Validator.kt
@@ -1,9 +1,112 @@
+import java.util.regex.Pattern
+
 abstract class Validator<T> {
     abstract fun validate(value: T?): List<ErrorCode>
 }
 
+class SnilsValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val result = mutableListOf<ErrorCode>()
+
+        value?.let {
+            with (it) {
+                if (!checkSymbols())
+                    result.add(ErrorCode.INVALID_CHARACTER)
+                if (!checkLength())
+                    result.add(ErrorCode.LENGTH)
+                try {
+                    if (isNeedToCheckNumber() && !checkNumber())
+                        result.add(ErrorCode.SNILS_CHECK_NUMBER)
+                } catch (t: Throwable) { }
+            }
+        } ?: result.add(ErrorCode.EMPTY_INPUT)
+
+        return result
+    }
+
+    private fun String.isNeedToCheckNumber(): Boolean = dropLast(2).toLong() > 1001998
+
+    private fun String.checkNumber(): Boolean =
+        dropLast(2).reversed().foldIndexed(0L) { index, acc, c ->
+            (acc + c.digitToInt() * (index + 1))
+        } % 101 % 100 == takeLast(2).toLong()
+
+    private fun String.checkSymbols(): Boolean = Pattern.matches("[0-9]+", this)
+
+    private fun String.checkLength(): Boolean = length == 11
+}
+
+class EmailValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val result = mutableListOf<ErrorCode>()
+
+        value?.let {
+            with (it) {
+                if (!checkSymbols())
+                    result.add(ErrorCode.INVALID_CHARACTER)
+                if (!checkLength())
+                    result.add(ErrorCode.LENGTH)
+                if (!checkDomain())
+                    result.add(ErrorCode.DOMAIN)
+            }
+        } ?: result.add(ErrorCode.EMPTY_INPUT)
+
+        return result
+    }
+
+    private fun String.checkSymbols(): Boolean = Pattern.matches("[A-Za-z@.]+", this)
+
+    private fun String.checkLength(): Boolean = length <= 32
+
+    private fun String.checkDomain(): Boolean = split("@")
+        .getOrElse(1) { "" }
+        .isNotEmpty()
+}
+
+class NameValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val result = mutableListOf<ErrorCode>()
+
+        value?.let {
+            with (it) {
+                if (!checkSymbols())
+                    result.add(ErrorCode.INVALID_CHARACTER)
+                if (!checkLength())
+                    result.add(ErrorCode.LENGTH)
+            }
+        } ?: result.add(ErrorCode.EMPTY_INPUT)
+
+        return result
+    }
+
+    private fun String.checkSymbols(): Boolean = Pattern.matches("[А-Яа-я]+", this)
+
+    private fun String.checkLength(): Boolean = length <= 16
+}
+
 class PhoneValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        return listOf(ErrorCode.INVALID_CHARACTER)
+        val result = mutableListOf<ErrorCode>()
+
+        value?.let {
+            with (it) {
+                if (!checkFirstSymbol() || !checkSymbols())
+                    result.add(ErrorCode.INVALID_CHARACTER)
+                if (!checkLength())
+                    result.add(ErrorCode.LENGTH)
+            }
+        } ?: result.add(ErrorCode.EMPTY_INPUT)
+
+        return result
     }
+
+    private fun String.checkFirstSymbol(): Boolean =
+        when (first()) {
+            '7', '8' -> true
+            else -> false
+        }
+
+    private fun String.checkSymbols(): Boolean = Pattern.matches("[0-9]+", this)
+
+    private fun String.checkLength(): Boolean = length == 11
 }
--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -8,13 +8,74 @@ import kotlin.test.assertNotNull
 class ClientServiceTest {
 
     private val gson = Gson()
-    private val clientService = ClientService()
+    private var clientService = ClientService()
+
+    //**Имя и Фамилия** - только кириллица, не более 16 символов каждое поле <br>
+    //**Телефон** - начинается с 7 или 8ки, только цифры, 11 знаков <br>
+    //**Email** - латиница, с валидацией @имя_домена, не более 32 символов <br>
+    //**СНИЛС** - только цифры, 11 символов, с валидацией Контрольного числа <br>
 
     @Test
     fun `success save client`() {
         val client = getClientFromJson("/success/user.json")
         val result = clientService.saveClient(client)
         assertNotNull(result)
+    }
+
+    @Test
+    fun `fail save client - data is empty`() {
+        val client = getClientFromJson("/fail/user_data_empty.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode.size, 5)
+        exception.errorCode.forEach {
+            assertEquals(it, ErrorCode.EMPTY_INPUT)
+        }
+    }
+
+    @Test
+    fun `fail save client - snils check number wrong`() {
+        val client = getClientFromJson("/fail/user_with_bad_snils.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode[0], ErrorCode.SNILS_CHECK_NUMBER)
+    }
+
+    @Test
+    fun `fail save client - data too long`() {
+        val client = getClientFromJson("/fail/user_data_too_long.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode
+            .filter {it == ErrorCode.LENGTH }
+            .size,
+            5
+        )
+    }
+
+    @Test
+    fun `fail save client - invalid characters`() {
+        val client = getClientFromJson("/fail/user_with_invalid_characters.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode
+            .filter {it == ErrorCode.INVALID_CHARACTER }
+            .size,
+            5
+        )
+    }
+
+    @Test
+    fun `fail save client - email domain missing`() {
+        val client = getClientFromJson("/fail/user_with_bad_email.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode[0], ErrorCode.DOMAIN)
     }
 
     @Test
@@ -31,7 +92,7 @@ class ClientServiceTest {
         val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+        assertEquals(exception.errorCode[0], ErrorCode.EMPTY_INPUT)
     }
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
