995aaac27a4cbbe07e4599d62583905b7883af9d
Поповцев Павел Владимирович <Popovtsev.P.Vl@sberbank.ru>
2022-10-16 14:12:22 +0300

    [Sprint-4][Exception task]

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,10 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_CHARACTER(100, "Недопустимый символ"),
+    INVALID_PHONE(110, "Недопустимый телефон!"),
+    INVALID_FIRST_NAME(120, "Недопустимое имя!"),
+    INVALID_LAST_NAME(130, "Недопустимая фамилия!"),
+    INVALID_EMAIL(140, "Недопустимый email!"),
+    INVALID_SNILS(150, "Недопустимый снилс!")
 }
