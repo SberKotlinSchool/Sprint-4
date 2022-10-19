5c3c9364cae031f268525aaa24034442f7efd189
arpineoganisian <arpineoganisian@gmail.com>
2022-10-19 03:41:00 +0300

    Функции и функциональное программирование

--- functional/src/main/kotlin/ru/sber/functional/Student.kt
+++ functional/src/main/kotlin/ru/sber/functional/Student.kt
@@ -3,10 +3,10 @@ package ru.sber.functional
 data class Student(
     val firstName: String,
     val lastName: String,
-    val middleName: String,
-    val age: Int,
+    val middleName: String = "No middleName is specified",
+    val age: Int = 0,
     val averageRate: Double,
-    val city: String,
-    val specialization: String,
-    val prevEducation: String?,
+    val city: String = "No city is specified",
+    val specialization: String = "No specialization is specified",
+    val prevEducation: String? = "No previous education is specified",
 )
