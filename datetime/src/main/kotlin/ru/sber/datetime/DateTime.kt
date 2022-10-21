76aba7b50b2129426e1d9e6959e3c77ef0b92956
arpineoganisian <arpineoganisian@gmail.com>
2022-10-21 23:33:04 +0300

    Date Time API

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,25 +1,39 @@
 package ru.sber.datetime
 
-import java.time.LocalDateTime
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.*
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    return ZoneOffset.getAvailableZoneIds()
+        .filter {
+            val offset = ZoneId.of(it).rules.getOffset(LocalDateTime.now()).toString()
+            offset.endsWith(":30") || offset.endsWith(":45")
+        }
+        .toSet()
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    return Month.values()
+        .map { LocalDate.of(year, it, 1)
+                .with(TemporalAdjusters.lastDayOfMonth()).dayOfWeek.toString() }
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    return Month.values()
+        .filter { LocalDate.of(year, it, 13).dayOfWeek == DayOfWeek.FRIDAY }
+        .size
 }
 
 // 4.
+//Вывести заданную дату в формате "01 Aug 2021, 23:39", в котором дата локализована для вывода в США (US)
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    return dateTime
+        .format(DateTimeFormatter.ofPattern("dd MMM yyy, HH:mm", Locale.US))
 }
 
 
