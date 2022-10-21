062653228b4fdd5b59972a9c31b30c1cee439c0c
arpineoganisian <arpineoganisian@gmail.com>
2022-10-21 17:48:21 +0300

    Generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,38 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <T> compare(p1: T, p2: T) = p1 == p2
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
-}
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T) = anArray.count { it > elem }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    var list = mutableListOf<T>()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
 
+    private var items : ArrayList<T> = ArrayList()
+
+    fun pop(): T {
+        require(items.isNotEmpty()) { "The stack is empty" }
+
+        val result = items.last()
+        items.remove(items.last())
+
+        return result
+    }
+
+    fun push(item: T) = items.add(item)
+
+    fun peek() = items.last()
+
+    fun isEmpty() = items.isEmpty()
 }
