1d6c3d3edeb91cff6a82ec8ef700267ec6f14bdc
Korshik <dakorshik@sberbank.ru>
2022-11-02 12:10:02 +0300

    Solve 1. Set of TimeZones

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,10 +1,17 @@
 package ru.sber.datetime
 
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneId
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val instant = Instant.parse("2022-01-01T00:00:00.00Z")
+    return ZoneId.getAvailableZoneIds()
+        .filter { zoneIdName ->
+            ZoneId.of(zoneIdName).rules.getOffset(instant).totalSeconds / 360 % 10 != 0
+        }
+        .toSet()
 }
 
 // 2.
