b00cc58ddc64a12dcae25d170a1baf10b1129008
Мосягин Стaнислав Владимирович <Mosyagin.S.V@sberbank.ru>
2022-11-07 10:54:56 +0500

    generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,41 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <K, V> compare(p1: Pair<K, V>, p2: Pair<K, V>): Boolean {
+    return p1 == p2
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.count { it > elem }
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    private val stack = mutableListOf<T>()
 
+    fun push(element: T) {
+        stack.add(element)
+    }
+
+    fun pop(): T? {
+        if (stack.isEmpty()) return null
+        val element = stack.last()
+        stack.removeLast()
+        return element
+    }
+
+    fun isEmpty(): Boolean {
+        return stack.isEmpty()
+    }
 }
