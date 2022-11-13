a2055cf95f68e4038870c0392e9c95e2ee1252be
zainulina.guzal <guzal.zainulina@gmail.com>
2022-11-13 21:03:47 +0300

    Sprint-4 Functional tasks

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,10 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
-    }
+    fun buildPowFunction(pow: Int): (Int) -> Long = {it.toDouble().pow(pow).toLong()}
 }
