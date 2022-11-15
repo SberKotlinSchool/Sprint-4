ffc397834741d322454e954f56103cdf570708a0
Aleksey Sypchenko <taler2002@mail.ru>
2022-11-15 12:29:26 +0300

    Sprint-4 Exceptions and validation

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,11 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_LENGTH(101, "Недопустимая длина поля"),
+    INVALID_NULL_VALUE(102, "Значение null"),
+    NOT_DIGITS_IN_PHONE_NUMBER(103, "Номер телефона должен состоять только из цифр"),
+    INVALID_FIRST_DIGIT(104, "Первая цифра номера должна быть 7 или 8"),
+    INVALID_EMAIL(105, "Невалидный адрес электронной почты"),
+    INVALID_CYRILLIC(106, "Недопустимы символы кроме кириллицы"),
+    INVALID_SNILS_CHECKSUM(107, "Невалидный СНИЛС")
 }
--- exception/src/main/kotlin/Validator.kt
+++ exception/src/main/kotlin/Validator.kt
@@ -1,9 +1,117 @@
+import java.util.regex.Pattern
+
 abstract class Validator<T> {
     abstract fun validate(value: T?): List<ErrorCode>
 }
 
 class PhoneValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        return listOf(ErrorCode.INVALID_CHARACTER)
+        if (value == null) {
+            return listOf(ErrorCode.INVALID_NULL_VALUE)
+        }
+
+        val errorList = mutableListOf<ErrorCode>()
+
+        if (value.isEmpty() || value.length != 11) {
+            errorList.add(ErrorCode.INVALID_LENGTH)
+        }
+
+        if (value.all { !it.isDigit() }) {
+            errorList.add(ErrorCode.NOT_DIGITS_IN_PHONE_NUMBER)
+        }
+
+        if (!value.startsWith("7") && !value.startsWith("8")) {
+            errorList.add(ErrorCode.INVALID_FIRST_DIGIT)
+        }
+
+        return errorList
+    }
+}
+
+class EmailValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        if (value == null) {
+            return listOf(ErrorCode.INVALID_NULL_VALUE)
+        }
+
+        val errorList = mutableListOf<ErrorCode>()
+
+        if (value.isEmpty() || value.length > 32) {
+            errorList.add(ErrorCode.INVALID_LENGTH)
+        }
+
+        if (!Pattern.compile(
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+                        + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+                        + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$"
+            ).matcher(value).matches()
+        ) {
+            errorList.add(ErrorCode.INVALID_EMAIL)
+        }
+
+        return errorList
+    }
+}
+
+class NameValidator() : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        if (value == null) {
+            return listOf(ErrorCode.INVALID_NULL_VALUE)
+        }
+
+        val errorList = mutableListOf<ErrorCode>()
+
+        if (value.isEmpty() || value.length > 16) {
+            errorList.add(ErrorCode.INVALID_LENGTH)
+        }
+
+        if (!value.toList().stream()
+                .allMatch { c: Char -> Pattern.compile("[а-яА-Я]").matcher(c.toString()).matches() }
+        ) {
+            errorList.add(ErrorCode.INVALID_CYRILLIC)
+        }
+
+        return errorList
+    }
+}
+
+class SnilsValidator() : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        if (value == null) {
+            return listOf(ErrorCode.INVALID_NULL_VALUE)
+        }
+
+        val errorList = mutableListOf<ErrorCode>()
+
+        if (value.isEmpty() || value.length != 11) {
+            errorList.add(ErrorCode.INVALID_LENGTH)
+        }
+
+        val snilsNumber = mutableListOf<Int>()
+        value.onEachIndexed { index, c -> snilsNumber.add(c.digitToInt()) }
+        val controlSum = ("" + value[9] + value[10]).toInt()
+        var sum = 0
+        var k = 9
+        for (i in 0..8) {
+            sum += snilsNumber[i] * k--
+        }
+
+        if (
+            (sum < 100 && sum != controlSum) || (sum == 100 && controlSum != 0)
+        ) {
+            errorList.add(ErrorCode.INVALID_SNILS_CHECKSUM)
+        } else if (sum > 100) {
+            val rest = sum % 101
+            if (rest == 100 && controlSum != 0) {
+                errorList.add(ErrorCode.INVALID_SNILS_CHECKSUM)
+            } else if (rest != controlSum) {
+                errorList.add(ErrorCode.INVALID_SNILS_CHECKSUM)
+            }
+        }
+
+        return errorList
     }
 }
