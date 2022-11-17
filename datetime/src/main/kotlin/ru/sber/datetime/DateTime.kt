5670f2105602ff82c0a5e6444e74ea5a79a66e68
Aleksandr Alekhin <AVlAlekhin@berbank.ru>
2022-11-18 02:38:33 +0300

    Sprint 3: datetime

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,26 +1,31 @@
 package ru.sber.datetime
 
+import java.time.DayOfWeek
 import java.time.LocalDateTime
+import java.time.Month
+import java.time.YearMonth
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+private const val SECONDS_IN_HOUR = 3_600
+private val US_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm").withLocale(Locale.US)
 
 // 1.
-fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
-}
+fun getZonesWithNonDivisibleByHourOffset(): Set<String> =
+    ZoneId.getAvailableZoneIds().sorted()
+        .filterNot { ZoneId.of(it).rules.getOffset(LocalDateTime.now()).totalSeconds % SECONDS_IN_HOUR == 0 }
+        .toSet()
 
 // 2.
-fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
-}
+fun getLastInMonthDayWeekList(year: Int): List<String> =
+    Month.values().map { month -> YearMonth.of(year, month).atEndOfMonth().dayOfWeek.name }
 
 // 3.
-fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
-}
+fun getNumberOfFridayThirteensInYear(year: Int): Int =
+    Month.values().map { month -> YearMonth.of(year, month).atDay(13) }
+        .count { it.dayOfWeek == DayOfWeek.FRIDAY }
 
 // 4.
-fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
-}
-
-
-
+fun getFormattedDateTime(dateTime: LocalDateTime): String =
+    dateTime.format(US_DATE_FORMATTER)
