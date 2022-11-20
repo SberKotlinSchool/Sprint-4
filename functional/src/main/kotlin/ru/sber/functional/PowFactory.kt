5a3a97ded0e9233d9f186ab983865b1b386657f8
Anna Popova <anna.popova201909@gmail.com>
2022-11-20 22:51:22 +0300

    buildPowFunction is ready

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,15 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(pow: Int): (Int) -> Int {
+        return { i: Int -> i.toDouble().pow(pow.toDouble()).toInt() }
     }
 }
+
+
+
