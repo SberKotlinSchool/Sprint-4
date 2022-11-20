0515f1a27769e29be0e819395f760880c2ef20c2
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2022-11-20 19:37:48 +0300

    Exception and function are done

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -4,7 +4,6 @@ object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
-    }
+
+    fun buildPowFunction(pow: Int): (Int) -> Int { return { v -> v.toDouble().pow(pow).toInt() } }
 }
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -2,9 +2,13 @@ package ru.sber.functional
 
 class StudentsGroup {
 
-    lateinit var students: List<Student>
+    private var students: List<Student> = listOf(
+        Student(firstName = "Дмитрий", lastName = "Иванов", averageRate = 1.0),
+        Student(firstName = "Антон", lastName = "Смирнов", averageRate = 2.0),
+        Student(firstName = "Лука", lastName = "Соболев", averageRate = 3.0),
+        Student(firstName = "Ефросинья", lastName = "Карибидиз", averageRate = 4.0),
+        Student(firstName = "Панкрат", lastName = "Черный", averageRate = 5.0)
+    )
 
-    fun filterByPredicate() {
-        TODO()
-    }
+    fun filterByPredicate(predicate: (Student) -> Boolean): List<Student> { return students.filter(predicate) }
 }
