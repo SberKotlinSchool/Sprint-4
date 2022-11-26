ecf7abe7127f79fec8316d81f33170efe176b7d2
Романова Ирина Дмитриевна <IDmRomanova@sberbank.ru>
2022-11-26 23:10:05 +0300

    generics and datetime

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,25 +1,38 @@
 package ru.sber.datetime
 
+import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.Month
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.Locale
+import java.util.TimeZone
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    return ZoneId.getAvailableZoneIds()
+        .map { TimeZone.getTimeZone(it) }
+        .filter { (it.rawOffset / 3600 % 1000) != 0 }
+        .map { it.id }.toSet()
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    return Month.values().map { LocalDate.of(year, it, 1) }
+        .map { it.with(TemporalAdjusters.lastDayOfMonth()).dayOfWeek.name }
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    return Month.values().map { LocalDate.of(year, it, 13) }
+        .count { it.dayOfWeek == DayOfWeek.FRIDAY }
 }
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm", Locale.US))
 }
 
 
