d630277b46b9897f543201d7b8f798332de66bf4
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2022-11-26 20:41:08 +0300

    Exceptions, functions and generics are done

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,9 +1,13 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
 
-    fun buildPowFunction(pow: Int): (Int) -> Int { return { v -> v.toDouble().pow(pow).toInt() } }
+    fun buildPowFunction(pow: Int): (Int) -> Int {
+        return { v -> v.toDouble().pow(pow).toInt() }
+    }
 }
