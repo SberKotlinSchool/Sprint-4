a41d65f0cdd45e53776aab197a5d0e1f8a98f003
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2022-11-26 19:53:32 +0300

    Exceptions, functions and generics are done

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,49 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
+import kotlin.collections.ArrayDeque
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <T> compare(p1: T, p2: T): Boolean {
+    return p1 == p2
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T: Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.filter { it > elem }.size
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T: Any>(vararg value: T){
+    val list: MutableList<T> = mutableListOf()
+
+    init {
+        list.addAll(value)
+    }
+
+    fun add(value: T) {
+        list.add(value)
+        list.sortBy { it.hashCode() }
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T : Any>(vararg value: T) {
+
+    private val stack: ArrayDeque<T> = ArrayDeque()
+
+    init {
+        stack.addAll(value)
+    }
+    fun isEmpty(): Boolean {
+        return stack.isEmpty()
+    }
+
+    fun pop(): T {
+        return stack.removeLast()
+    }
+
+    fun push(value: T) {
+        stack.add(value)
+    }
 
 }
