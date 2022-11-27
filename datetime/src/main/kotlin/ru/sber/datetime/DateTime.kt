137175ac8ef9d6b15fb738cfad7d55cc92f2daf6
16939749 / astafyev-ai <astafyev.a.igor@sberbank.ru>
2022-11-27 22:25:02 +0300

    Spring4 - выполнено задание на тему "datetime"

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,26 +1,55 @@
 package ru.sber.datetime
 
-import java.time.LocalDateTime
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val secondsInHour = 3600
+    return ZoneId.getAvailableZoneIds()
+        .filterNot {
+            val secondInZone = ZoneId.of(it)
+                .rules
+                .getOffset(LocalDateTime.now())
+                .totalSeconds
+            secondInZone % secondsInHour == 0
+        }
+        .sorted()
+        .toSet()
+}
+
+fun main() {
+    println(getZonesWithNonDivisibleByHourOffset())
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    val list = mutableListOf<String>()
+    val currentYear = Year.of(year)
+
+    for (month in 1..12) {
+        val lengthOfMonth = currentYear.atMonth(month).lengthOfMonth()
+        list.add(LocalDate.of(year, month, lengthOfMonth).dayOfWeek.name)
+    }
+    return list
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    var countDays = 0
+    for (month in 1..12) {
+        val dayOfWeek = LocalDate.of(year, month, 13).dayOfWeek
+        if (dayOfWeek == DayOfWeek.FRIDAY) {
+            countDays++
+        }
+    }
+    return countDays
 }
 
 // 4.
-fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
-}
-
-
-
+fun getFormattedDateTime(dateTime: LocalDateTime): String = dateTime.format(
+    DateTimeFormatter
+        .ofPattern("dd MMM yyyy, HH:mm")
+        .withLocale(Locale.US)
+)
