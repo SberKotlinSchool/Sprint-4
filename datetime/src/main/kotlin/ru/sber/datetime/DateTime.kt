762a95fa8d72d6b91945048d687396b83e35e953
Воротов Алексей Викторович <Vorotov.A.Vikt@sberbank.ru>
2022-11-27 19:17:43 +0300

    Спринт 4. Задачи на Date/Time

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,26 +1,50 @@
 package ru.sber.datetime
 
+import java.time.DayOfWeek.FRIDAY
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalDateTime.now
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.time.temporal.TemporalAdjusters.dayOfWeekInMonth
+import java.util.*
+
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    return ZoneId.getAvailableZoneIds().filter {
+        ZoneId.of(it).rules.getOffset(now()).totalSeconds % (60 * 60) != 0
+    }.toSet()
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    val lastDayOfMonthAdjuster = TemporalAdjusters.lastDayOfMonth()
+    return (1 .. 12)
+        .map { LocalDate.of(year,it,1) }
+        .map { lastDayOfMonthAdjuster.adjustInto(it)}
+        .map { LocalDate.from(it).dayOfWeek.toString()}
+        .toList()
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    val fta = dayOfWeekInMonth(1, FRIDAY)
+    var d = LocalDate.from(fta.adjustInto(LocalDate.of(year, 1, 1)))
+    var result = 0
+    while (d.year <= year) {
+        if (d.dayOfMonth == 13) {
+            result++
+        }
+        d = d.plusDays(7)
+    }
+    return result
 }
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    val fmt = DateTimeFormatter.ofPattern("dd MMM u, HH:mm").withLocale(Locale.US)
+    return dateTime.format(fmt)
 }
-
-
 
