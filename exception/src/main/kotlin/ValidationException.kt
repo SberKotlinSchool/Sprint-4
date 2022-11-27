bdf3ae29af9e4f8274da5e06c4ab7584a9b354f9
16939749 / astafyev-ai <astafyev.a.igor@sberbank.ru>
2022-11-27 11:16:14 +0300

    Spring4 - выполнено задание на тему "exception"

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,25 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    MISSING_PHONE(110, "Не заполнено обязательное поле: номер телефона"),
+    INVALID_PHONE_FORMAT(111, "Номер телефон должен состоять из 11 цифр и начинаться с 7 или 8"),
+
+    MISSING_FIRST_NAME(121, "Не заполнено обязательное поле: имя"),
+    INVALID_FIRST_NAME_FORMAT(120, "Имя должно содержать только кириллицу, не более 16 символов"),
+
+    MISSING_LAST_NAME(130, "Не заполнено обязательное поле: фамилия"),
+    INVALID_LAST_NAME_FORMAT(131, "Фамилия должна содержать только кириллицу, не более 16 символов"),
+
+    MISSING_EMAIL(140, "Не заполнено обязательное поле: емейл"),
+    INVALID_EMAIL_FORMAT(
+        141,
+        "Емейл должен состоять из латинских букв, не более 16 символов, а также иметь @имя_домена"
+    ),
+
+    MISSING_SNILS(150, "Не заполнено обязательное поле: емейл"),
+    INVALID_SNILS_FORMAT(
+        151,
+        "Емейл должен состоять из латинских букв, не более 16 символов, а также иметь @имя_домена"
+    ),
+    INVALID_SNILS_CHECKSUM(152, "Емейл содержит некорректное контрольное число")
 }
--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class ClientServiceTest {
 
@@ -18,25 +19,66 @@ class ClientServiceTest {
     }
 
     @Test
-    fun `fail save client - validation error`() {
+    fun `fail save client - phone validation error`() {
         val client = getClientFromJson("/fail/user_with_bad_phone.json")
-        assertThrows<ValidationException>("Ожидаемая ошибка") {
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
+        assertEquals(1, exception.errorCode.size)
+        assertTrue { ErrorCode.INVALID_PHONE_FORMAT in exception.errorCode }
     }
 
     @Test
-    fun `fail save client - validation errors`() {
+    fun `fail save client - all fields is corrupted`() {
         val client = getClientFromJson("/fail/user_data_corrupted.json")
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(5, exception.errorCode.size)
+        assertTrue { ErrorCode.INVALID_FIRST_NAME_FORMAT in exception.errorCode }
+        assertTrue { ErrorCode.INVALID_LAST_NAME_FORMAT in exception.errorCode }
+        assertTrue { ErrorCode.INVALID_PHONE_FORMAT in exception.errorCode }
+        assertTrue { ErrorCode.INVALID_SNILS_FORMAT in exception.errorCode }
+        assertTrue { ErrorCode.INVALID_EMAIL_FORMAT in exception.errorCode }
+    }
+
+    @Test
+    fun `fail save client - snils with invalid checksum`() {
+        val client = getClientFromJson("/fail/user_with_bad_snils_checksum.json")
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(1, exception.errorCode.size)
+        assertTrue { ErrorCode.INVALID_SNILS_CHECKSUM in exception.errorCode }
+    }
+
+    @Test
+    fun `fail save client - all fields is missing`() {
+        val client = getClientFromJson("/fail/user_with_empty_fields.json")
         val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+        assertEquals(5, exception.errorCode.size)
+        assertTrue { ErrorCode.MISSING_FIRST_NAME in exception.errorCode }
+        assertTrue { ErrorCode.MISSING_LAST_NAME in exception.errorCode }
+        assertTrue { ErrorCode.MISSING_PHONE in exception.errorCode }
+        assertTrue { ErrorCode.MISSING_EMAIL in exception.errorCode }
+        assertTrue { ErrorCode.MISSING_SNILS in exception.errorCode }
+    }
+
+    @Test
+    fun `fail save client - first-last names validation error`() {
+        val client = getClientFromJson("/fail/user_with_bad_name.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(2, exception.errorCode.size)
+        assertTrue { ErrorCode.INVALID_FIRST_NAME_FORMAT in exception.errorCode }
+        assertTrue { ErrorCode.INVALID_LAST_NAME_FORMAT in exception.errorCode }
     }
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
         .takeIf { it != null }
         ?.let { gson.fromJson(it.readText(), Client::class.java) }
         ?: throw Exception("Что-то пошло не так))")
-
 }
