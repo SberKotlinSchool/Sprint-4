87d69e494225d817ea85db79caf99f056e7060b3
Anna Popova <anna.popova201909@gmail.com>
2022-11-27 19:55:12 +0300

    ready for PR

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,9 +1,6 @@
 package ru.sber.datetime
 
-import java.time.DayOfWeek
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.Month
+import java.time.*
 import java.time.format.DateTimeFormatter
 import java.time.temporal.TemporalAdjusters
 import java.util.*
@@ -11,8 +8,23 @@ import java.util.*
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    var result = mutableSetOf<String>()
+
+    val availableZoneIds = ZoneId.getAvailableZoneIds()
+    val localDateTime = LocalDateTime.now()
+
+    for (a in availableZoneIds) {
+        val zonedDateTime = localDateTime.atZone(ZoneId.of(a))
+        val zoneOffset = zonedDateTime.offset
+        val offset: String = zoneOffset.id.replace("Z", "+00:00")
+        val substringAfter = offset.substringAfter(":")
+        if (substringAfter != "00") {
+            result.add(ZoneId.of(a).toString())
+        }
+    }
+    return result.toSortedSet()
 }
+
 
 fun getLastInMonthDayWeekList(year: Int): List<String> {
     var daysList = arrayListOf<String>()
