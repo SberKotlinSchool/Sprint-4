15019ba11d8c2e0d0686cdf03eb1480c1e43047e
Воротов Алексей Викторович <Vorotov.A.Vikt@sberbank.ru>
2022-11-28 22:58:41 +0300

    Спринт 4. Функции

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -2,9 +2,11 @@ package ru.sber.functional
 
 class StudentsGroup {
 
-    lateinit var students: List<Student>
+    private val students: List<Student> = listOf(
+        Student(firstName = "Тихомир", lastName = "Ярополкович", averageRate = 5.1),
+        Student(firstName = "Светозар", lastName = "Святославич", averageRate = 5.2),
+        Student(firstName = "Ратибор", lastName = "Всеволодович", averageRate = 5.3)
+    )
 
-    fun filterByPredicate() {
-        TODO()
-    }
+    fun filterByPredicate(predicate: (Student) -> Boolean): Collection<Student> = students.filter(predicate)
 }
