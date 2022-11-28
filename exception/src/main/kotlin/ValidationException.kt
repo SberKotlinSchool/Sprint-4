9aded2bf51a68b1d8cd43833606f1c48797fb2e0
Mr_Rybkin <dyxa145@gmail.com>
2022-11-28 20:58:04 +0300

    exception

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,10 @@
-class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
+class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",\n") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_FORMAT_NAME(100, "Имя - только кириллица, не более 16 символов каждое поле"),
+    INVALID_FORMAT_PHONE(101, "Телефон - начинается с 7 или 8ки, только цифры, 11 знаков"),
+    INVALID_FORMAT_EMAIL(102, "Емайл - латиница, с валидацией @имя_домена, не более 32 символов"),
+    INVALID_FORMAT_SNILS(103, "СНИЛС - только цифры, 11 символов"),
+    INVALID_FORMAT_SNILS_SUM(104, "СНИЛС - Ошибка валидации контрольного числа"),
+    STRING_NULL_OR_BLANK(105, "Строка пустая или равна null"),
 }
--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -18,20 +18,57 @@ class ClientServiceTest {
     }
 
     @Test
-    fun `fail save client - validation error`() {
+    fun `fail save client - validation phone error`() {
         val client = getClientFromJson("/fail/user_with_bad_phone.json")
-        assertThrows<ValidationException>("Ожидаемая ошибка") {
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
+        assertEquals(exception.message, ErrorCode.INVALID_FORMAT_PHONE.msg)
     }
 
     @Test
-    fun `fail save client - validation errors`() {
-        val client = getClientFromJson("/fail/user_data_corrupted.json")
+    fun `fail save client - validation name errors`() {
+        val client = getClientFromJson("/fail/user_with_bad_first_name.json")
         val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+        assertEquals(exception.errorCode[0].msg, ErrorCode.INVALID_FORMAT_NAME.msg)
+    }
+
+    @Test
+    fun `fail save client - validation email error`() {
+        val client = getClientFromJson("/fail/user_with_bad_email.json")
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.message, ErrorCode.INVALID_FORMAT_EMAIL.msg)
+    }
+
+    @Test
+    fun `fail save client - validation snils error`() {
+        val client = getClientFromJson("/fail/user_with_bad_snils.json")
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.message, ErrorCode.INVALID_FORMAT_SNILS.msg)
+    }
+
+    @Test
+    fun `fail save client - validation snils sum error`() {
+        val client = getClientFromJson("/fail/user_with_bad_snils_sum.json")
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.message, ErrorCode.INVALID_FORMAT_SNILS_SUM.msg)
+    }
+
+    @Test
+    fun `fail save client - validation null error`() {
+        val client = getClientFromJson("/fail/user_with_value_null.json")
+        val exception = assertThrows<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.message, ErrorCode.STRING_NULL_OR_BLANK.msg)
     }
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
