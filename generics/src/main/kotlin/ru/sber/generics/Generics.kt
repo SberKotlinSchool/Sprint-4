9aed67cd26f549e7aa8bea7418f3abccc83e7435
SlumdogProgrammer <bolotin99@inbox.ru>
2023-01-09 00:32:41 +0300

    GENERICS done

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,31 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
+// по моему, лишние импорты из-за которых сборка не проходит
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <K, V> compare(p1: Pair<K, V>, p2: Pair<K, V>): Boolean {
+    return p1.first == p2.first && p1.second == p2.second
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.count { it > elem }
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
-
+class Stack<T> {
+    private val stack = arrayListOf<T>()
+    fun isEmpty() = stack.isEmpty()
+    fun push(value: T) = stack.add(value)
+    fun pop() = stack.removeLastOrNull()
 }
