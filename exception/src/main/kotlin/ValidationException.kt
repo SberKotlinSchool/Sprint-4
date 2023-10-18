02156a2ba451d5743610c568beebfa9330161f39
popla <poplavski79@gmail.com>
2023-10-19 00:53:15 +0700

    Sprint 4. Exception

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,9 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
+    INVALID_CHARACTER(100, "Недопустимый символ"),
+    INVALID_LENGTH( 101, "Некорректная длина" ),
+    INVALID_FORMAT( 102, "Неверный формат" ),
+    INVALID_SUM_SNILS(103, "Неверная контрольная сумма СНИЛС" )
     // ...
 }
