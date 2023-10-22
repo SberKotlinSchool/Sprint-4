b9e32e1227b80995bf394d6c96c961cfc1c3e0a3
Yury Gorbunov <YVGorbunov@sberbank.ru>
2023-10-22 20:59:56 +0300

    Sprint-4. Generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,30 @@
 package ru.sber.generics
 
-//import com.sun.org.apache.xpath.internal.operations.Bool
-//import java.util.*
-
-// 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
-
-// 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <K, V> compare(p1: Pair<K, V>, p2: Pair<K, V>): Boolean {
+//    return (p1 == p2)
+    return ((p1.first == p2.first) && (p1.second == p2.second))
 }
 
-// 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.filterIndexed { _, it -> it > elem }.size
 }
 
-// 4.
-class Stack {
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+            .also { list.sort() }
+    }
+}
+
+class Stack<T : Comparable<T>> {
+
+    val list: MutableList<T> = mutableListOf()
+    fun push(value: T) = list.add(value)
+
+    fun pop(): T = list.removeLast()
+
+    fun isEmpty(): Boolean = list.isEmpty()
 
 }
--- generics/src/test/kotlin/ru/sber/generics/GenericsTest.kt
+++ generics/src/test/kotlin/ru/sber/generics/GenericsTest.kt
@@ -8,35 +8,43 @@ class GenericsTest {
 
     @Test
     fun comparePairsTest() {
-//        assertTrue(compare(Pair("Hello", 2L), Pair("Hello", 2L)))
-//        assertFalse(compare(Pair(BigDecimal(5), false), Pair(BigDecimal(5), true)))
+        assertTrue(compare(Pair("Hello", 2L), Pair("Hello", 2L)))
+        assertFalse(compare(Pair(BigDecimal(5), false), Pair(BigDecimal(5), true)))
     }
 
     @Test
     fun countGreaterThanTest() {
-//        assertEquals(2, countGreaterThan(arrayOf(5, 4, 3, 2, 1), 3))
-//        assertEquals(2, countGreaterThan(arrayOf('a', 'b', 'c', 'd', 'e'), 'c'))
+        assertEquals(2, countGreaterThan(arrayOf(5, 4, 3, 2, 1), 3))
+        assertEquals(2, countGreaterThan(arrayOf('a', 'b', 'c', 'd', 'e'), 'c'))
+
+        assertEquals(4, countGreaterThan(arrayOf('A', 'b', 'c', 'D', 'e', 'F'), 'E'))
     }
 
     @Test
     fun sorterTest() {
-//        val sorter = Sorter<String>()
-//        sorter.add("foo")
-//        sorter.add("hello")
-//        sorter.add("bar")
-//
-//        assertEquals(listOf("bar", "foo", "hello"), sorter.list)
+        val sorter = Sorter<String>()
+        sorter.add("foo")
+        sorter.add("hello")
+        sorter.add("bar")
+        assertEquals(listOf("bar", "foo", "hello"), sorter.list)
+
+        sorter.list.clear()
+        sorter.add("5")
+        sorter.add("1")
+        sorter.add("3")
+        sorter.add("8")
+        assertEquals(listOf("1", "3", "5", "8"), sorter.list)
     }
 
     @Test
     fun stackTest() {
-//        val stack = Stack<Int>()
-//        assertTrue(stack.isEmpty())
-//
-//        stack.push(5)
-//        stack.push(3)
-//        assertEquals(3, stack.pop())
-//        assertFalse(stack.isEmpty())
-//        assertEquals(5, stack.pop())
+        val stack = Stack<Int>()
+        assertTrue(stack.isEmpty())
+
+        stack.push(5)
+        stack.push(3)
+        assertEquals(3, stack.pop())
+        assertFalse(stack.isEmpty())
+        assertEquals(5, stack.pop())
     }
 }
