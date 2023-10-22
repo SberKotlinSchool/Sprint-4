30d10b5fe5abf05ed0003e5146ee2ab001d44bec
DataBricker <ilsmag47@gmail.com>
2023-10-22 23:04:19 +0300

    sprint 4

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,68 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_CHARACTER(100, "Invalid symbol"),
+    EMPTY_FIELD(101, "Empty field"),
+    INVALID_LENGTH(102, "Invalid len"),
+    NULL_FIELD(103, "Field doesn't set"),
+    INVALID_CONTROL_NUMBER(104, "Invalid SNILS")
+}
+
+interface Validation<E> {
+    fun validate(data: E): ErrorCode?
+}
+
+class NotEmptyStringValidation : Validation<String> {
+    override fun validate(data: String): ErrorCode? {
+        if (data.trim() == "")
+            return ErrorCode.EMPTY_FIELD
+        return null
+    }
+}
+
+class ExactLengthStringValidation(private val expectedLength: Int) : Validation<String> {
+    override fun validate(data: String): ErrorCode? {
+        if (data.length != expectedLength)
+            return ErrorCode.INVALID_LENGTH
+        return null
+    }
+}
+
+class MatchRegexStringValidation(private val expectedRegex: String) : Validation<String> {
+    override fun validate(data: String): ErrorCode? {
+        if (!data.matches(Regex(expectedRegex)))
+            return ErrorCode.INVALID_CHARACTER
+        return null
+    }
+}
+
+class MaxLengthStringValidation(private val maxLength: Int) : Validation<String> {
+    override fun validate(data: String): ErrorCode? {
+        if (data.length > maxLength)
+            return ErrorCode.INVALID_LENGTH
+        return null
+    }
+}
+
+class SnilsControlNumberStringValidation : Validation<String> {
+    override fun validate(data: String): ErrorCode? {
+        if (!data.matches(Regex("\\d{11}")))
+            return null
+
+        val len = 9
+        val calculated = getNum(data)
+
+        val real = data.substring(len, data.length).toInt()
+
+        if (calculated != real)
+            return ErrorCode.INVALID_CONTROL_NUMBER
+
+        return null
+    }
+
+    private fun getNum(data: String, len: Int = 9) =
+        data.substring(0, len).split("")
+        .filter { it.isNotEmpty() }
+        .mapIndexed { i, s -> (len - i) * s.toInt() }
+        .sum().mod(101)
 }
--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -1,13 +1,17 @@
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.junit.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 
 class ClientServiceTest {
 
-    private val gson = Gson()
     private val clientService = ClientService()
 
     @Test
@@ -17,21 +21,42 @@ class ClientServiceTest {
         assertNotNull(result)
     }
 
-    @Test
-    fun `fail save client - validation error`() {
-        val client = getClientFromJson("/fail/user_with_bad_phone.json")
-        assertThrows<ValidationException>("Ожидаемая ошибка") {
-            clientService.saveClient(client)
-        }
+    @ParameterizedTest
+    @MethodSource("getClientsWithInvalidPhone")
+    fun `fail save client - with invalid phone`(client: Client, expectedErrors: Array<ErrorCode>) {
+        testCommonBody(client, expectedErrors)
     }
 
-    @Test
-    fun `fail save client - validation errors`() {
-        val client = getClientFromJson("/fail/user_data_corrupted.json")
-        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+    @ParameterizedTest
+    @MethodSource("getClientsWithInvalidFirstName")
+    fun `fail save client - with invalid first name`(client: Client, expectedErrors: Array<ErrorCode>) {
+        testCommonBody(client, expectedErrors)
+    }
+
+    @ParameterizedTest
+    @MethodSource("getClientsWithInvalidLastName")
+    fun `fail save client - with invalid last name`(client: Client, expectedErrors: Array<ErrorCode>) {
+        testCommonBody(client, expectedErrors)
+    }
+
+    @ParameterizedTest
+    @MethodSource("getClientsWithInvalidEmail")
+    fun `fail save client - with invalid email`(client: Client, expectedErrors: Array<ErrorCode>) {
+        testCommonBody(client, expectedErrors)
+    }
+
+    @ParameterizedTest
+    @MethodSource("getClientsWithInvalidSnils")
+    fun `fail save client - with invalid snils`(client: Client, expectedErrors: Array<ErrorCode>) {
+        testCommonBody(client, expectedErrors)
+    }
+
+    private fun testCommonBody(client: Client, expectedErrors: Array<ErrorCode>) {
+        val actual = assertFailsWith<ValidationException> {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+
+        assertContentEquals(expectedErrors, actual.errorCode)
     }
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
@@ -39,4 +64,47 @@ class ClientServiceTest {
         ?.let { gson.fromJson(it.readText(), Client::class.java) }
         ?: throw Exception("Что-то пошло не так))")
 
+    companion object {
+        private val gson = Gson()
+
+        @JvmStatic
+        fun getClientsWithInvalidPhone(): List<Arguments> =
+            getClientsFromJson("/fail/user_with_bad_phone.json")
+
+        @JvmStatic
+        fun getClientsWithInvalidFirstName(): List<Arguments> =
+            getClientsFromJson("/fail/user_with_bad_firstname.json")
+
+        @JvmStatic
+        fun getClientsWithInvalidLastName(): List<Arguments> =
+            getClientsFromJson("/fail/user_with_bad_lastname.json")
+
+        @JvmStatic
+        fun getClientsWithInvalidEmail(): List<Arguments> =
+            getClientsFromJson("/fail/user_with_bad_email.json")
+
+        @JvmStatic
+        fun getClientsWithInvalidSnils(): List<Arguments> =
+            getClientsFromJson("/fail/user_with_bad_snils.json")
+
+        private fun getClientsFromJson(fileName: String): List<Arguments> =
+            ClientServiceTest::class.java.getResource(fileName)
+                .takeIf { it != null }
+                ?.let {
+                    val targetClassToken = object : TypeToken<Array<Map<String, Any>>>() {}.type
+                    gson.fromJson(it.readText(), targetClassToken) as Array<Map<String, Any>>
+                }
+                ?.map {
+                    val client = fromMapToObject<Client>(it["client"])
+                    val errors = fromMapToObject<Array<ErrorCode>>(it["errors"])
+                    Arguments.of(client, errors)
+                }
+                ?.toList()
+                ?: throw Exception("Что-то пошло не так))")
+
+        private inline fun <reified T> fromMapToObject(client: Any?): T {
+            val clientJson = gson.toJson(client)
+            return gson.fromJson(clientJson, T::class.java)
+        }
+    }
 }
