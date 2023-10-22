a9974a6dc08f998a8a61238f332f56a96e2324da
ShedowSith <rahimyanov_aleks@mail.ru>
2023-10-22 13:23:41 +0300

    реализованы функции и написан тест

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -4,7 +4,11 @@ object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(pow: Int) = { number: Int ->
+        var result = 1
+        repeat(pow) {
+            result *= number
+        }
+        result
     }
 }
--- functional/src/test/java/ru/sber/functional/PowFactoryTest.kt
+++ functional/src/test/java/ru/sber/functional/PowFactoryTest.kt
@@ -6,7 +6,24 @@ import org.junit.jupiter.api.Test
 class PowFactoryTest {
     @Test
     fun `buildPowFunction should return lambda It should calculate to second power`() {
-        // expect
-        // assertEquals(9, PowFactory.buildPowFunction(2)(3))
+         assertEquals(9, PowFactory.buildPowFunction(2)(3))
+    }
+
+    @Test
+    fun `filtering students average scores`() {
+        val studentsGroup = StudentsGroup()
+        studentsGroup.students = listOf(
+                Student(firstName = "Иван", lastName = "Иванов", averageRate = 4.0),
+                Student(firstName = "Петр", lastName = "Петров", averageRate = 3.0),
+                Student(firstName = "Роман", lastName = "Романов", averageRate = 5.0),
+                Student(firstName = "Оксана", lastName = "Кучкова", averageRate = 4.5)
+        )
+
+        val filterStudent = studentsGroup.filterByPredicate { student: Student -> student.averageRate > 4.0 }
+
+        assertEquals(filterStudent, listOf(
+                Student(firstName = "Роман", lastName = "Романов", averageRate = 5.0),
+                Student(firstName = "Оксана", lastName = "Кучкова", averageRate = 4.5))
+        )
     }
 }
