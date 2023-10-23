90c64acb2cbdfe68d4213722e1684ca305751df3
Firebat <flamebat@mail.ru>
2023-10-23 19:28:40 +0300

    Sprint-4 beforeAll method

--- functional/src/test/java/ru/sber/functional/StudentsGroupTest.kt
+++ functional/src/test/java/ru/sber/functional/StudentsGroupTest.kt
@@ -1,26 +1,33 @@
 package ru.sber.functional
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StudentsGroupTest {
     private val studentsGroup = StudentsGroup()
+    private lateinit var student1: Student
+    private lateinit var student2: Student
+    private lateinit var student3: Student
 
-    // FIXME не удалось заставить работать BeforeAll
-    private val student1 = Student("Иван", "Иванов", "Иванович", 19, 3.7)
-    private val student2 =
-        Student(city = "Москва", firstName = "Петр", lastName = "Петров", averageRate = 4.7)
-    private val student3 = Student(
-        city = "Москва",
-        firstName = "Сидор",
-        lastName = "Сидоров",
-        averageRate = 4.8,
-        middleName = "Сидорович",
-        prevEducation = "Школа",
-        specialization = "Финансы",
-        age = 20
-    )
+    @BeforeAll
+    fun setUpAll() {
+        student1 = Student("Иван", "Иванов", "Иванович", 19, 3.7)
+        student2 = Student(city = "Москва", firstName = "Петр", lastName = "Петров", averageRate = 4.7)
+        student3 = Student(
+            city = "Москва",
+            firstName = "Сидор",
+            lastName = "Сидоров",
+            averageRate = 4.8,
+            middleName = "Сидорович",
+            prevEducation = "Школа",
+            specialization = "Финансы",
+            age = 20
+        )
+    }
 
     @BeforeEach
     fun setUp() {
