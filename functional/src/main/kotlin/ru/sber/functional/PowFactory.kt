76b634ac2ddd4b0b7d2d4ab889eaece987da6c48
popla <poplavski79@gmail.com>
2023-10-24 01:43:20 +0700

    Sprint 4. fucntion уточнение

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,14 +1,40 @@
 package ru.sber.functional
 
+import java.awt.geom.QuadCurve2D
 import kotlin.math.pow
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) = { value: Int -> value.toDouble().pow(pow).toInt() }
-}
+//    fun buildPowFunction(pow: Int) = { value: Int -> value.toDouble().pow(pow) }
 
+    fun buildPowFunction(pow: Int) = { value: Int -> when{
+        ( pow > 0 ) -> IntRange(1, pow ).fold(1.toDouble()) { a, b -> a * value }
+        ( pow < 0 ) -> IntRange( pow + 1 , 0 ).fold(1.toDouble()) { a, b -> a / value }
+        else -> 1.0
+    } }
+
+    fun buildPowFunctionReqursive(pow: Int): (Int) -> Double{
+
+        var localPow = pow
+
+        fun myPow(value: Int): Double{
+            if (localPow > 0 ) {
+                localPow -= 1
+                return value * myPow(value)
+            }
+            else if (localPow < 0 ) {
+                    localPow += 1
+                    return 1.0 / ( value / myPow(value) )
+            }
+            else return 1.0
+
+        }
+
+        return ::myPow
+    }
+}
 fun main(){
 
-    println(PowFactory.buildPowFunction(3)(2))
+    println(PowFactory.buildPowFunction(-2)(4))
 }
--- functional/src/test/java/ru/sber/functional/PowFactoryTest.kt
+++ functional/src/test/java/ru/sber/functional/PowFactoryTest.kt
@@ -2,11 +2,43 @@ package ru.sber.functional
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.math.pow
 
 class PowFactoryTest {
     @Test
     fun `buildPowFunction should return lambda It should calculate to second power`() {
         // expect
-         assertEquals(9, PowFactory.buildPowFunction(2)(3))
+         assertEquals(3.0.pow(2), PowFactory.buildPowFunction(2)(3))
     }
+
+    @Test
+    fun `buildPowFunction should power 0`() {
+        // expect
+        assertEquals(3.0.pow(0), PowFactory.buildPowFunction(0)(3))
+    }
+
+    @Test
+    fun `buildPowFunction should negative power`() {
+        // expect
+        assertEquals(2.0.pow(-4), PowFactory.buildPowFunction(-4)(2))
+    }
+    @Test
+    fun `buildPowFunctionReqursive should return lambda It should calculate to second power`() {
+        // expect
+        assertEquals(3.0.pow(2), PowFactory.buildPowFunctionReqursive(2)(3))
+    }
+
+    @Test
+    fun `buildPowFunctionReqursive should power 0`() {
+        // expect
+        assertEquals(3.0.pow(0), PowFactory.buildPowFunctionReqursive(0)(3))
+    }
+
+    @Test
+    fun `buildPowFunctionReqursive should negative power`() {
+        // expect
+        assertEquals(2.0.pow(-4), PowFactory.buildPowFunctionReqursive(-4)(2))
+    }
+
+
 }
