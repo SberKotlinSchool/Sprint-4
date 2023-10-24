84367526484581076b3b8cbb0272d91db9c93190
avkononenko <avkononenko@domclick.ru>
2023-10-25 01:16:48 +0300

    задание к спринту 3 - add tests for functional section

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -8,11 +8,3 @@ object PowFactory {
      */
     fun buildPowFunction(pow: Int) = { n: Number -> n.toDouble().pow(pow) }
 }
-
-fun main() {
-    val powFunc = PowFactory.buildPowFunction(10)
-
-    println(powFunc(2L))
-    println(powFunc(3))
-    println(PowFactory.buildPowFunction(2)(3))
-}
