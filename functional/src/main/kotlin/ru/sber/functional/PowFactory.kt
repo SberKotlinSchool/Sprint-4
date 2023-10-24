5a17811fb68c0902cf6418388a7919a89b934003
avkononenko <avkononenko@domclick.ru>
2023-10-25 00:58:02 +0300

    задание к спринту 3 - секция functional

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,18 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
      */
-    fun buildPowFunction(pow: Int) {
-        TODO()
-    }
+    fun buildPowFunction(pow: Int) = { n: Number -> n.toDouble().pow(pow) }
+}
+
+fun main() {
+    val powFunc = PowFactory.buildPowFunction(10)
+
+    println(powFunc(2L))
+    println(powFunc(3))
+    println(PowFactory.buildPowFunction(2)(3))
 }
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,22 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(predicate: (Student) -> Boolean): List<Student> = students.filter(predicate)
+}
+
+fun main() {
+    val group = StudentsGroup().apply {
+        students = listOf(
+            Student(firstName = "Кирилл", lastName = "Иванов", averageRate = 4.4),
+            Student(firstName = "Иван", lastName = "Сергеев", averageRate = 3.5),
+            Student(firstName = "Анна", lastName = "Петрова", city = "Москва", averageRate = 4.8),
+            Student(firstName = "Алексей", lastName = "Курочкин", averageRate = 3.1)
+        )
     }
+
+    group.filterByPredicate {
+        it.averageRate > 4
+    }
+        .joinToString("\n")
+        .also { println(it) }
 }
