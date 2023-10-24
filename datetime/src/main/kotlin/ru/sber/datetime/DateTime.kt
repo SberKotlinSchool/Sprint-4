b127f88ad3721375e606d1c7a1d73c50d420a77f
Федор Филиппов <fefilippov@edu.hse.ru>
2023-10-24 22:39:48 +0300

    datetime task done

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,25 +1,42 @@
 package ru.sber.datetime
 
-import java.time.LocalDateTime
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val filteredSet = ZoneId.getAvailableZoneIds()
+        .filter {
+            ZoneId.of(it).rules.getOffset(LocalDateTime.now(ZoneOffset.UTC)).totalSeconds % 3600 != 0
+        }
+        .toSet()
+
+    return filteredSet
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    val result = (1..12).map { month ->
+        YearMonth.of(year, month).atEndOfMonth().dayOfWeek.toString()
+    }
+        .toList()
+
+    return result
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    val result = (1..12).map { month ->
+        LocalDate.of(year, month, 13)
+    }
+        .count { it.dayOfWeek == DayOfWeek.FRIDAY }
+    return result
 }
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    return dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm", Locale.US))
 }
 
 
