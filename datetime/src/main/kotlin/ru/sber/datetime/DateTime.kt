d4cd962f56f78dff222bd211f898bd57a5153319
avkononenko <avkononenko@domclick.ru>
2023-10-29 00:29:45 +0300

    задание к спринту 3 - datetime, generics

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,25 +1,50 @@
 package ru.sber.datetime
 
+import java.time.DayOfWeek
+import java.time.Duration
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.*
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val now = LocalDateTime.now()
+    val greenwichTime = ZonedDateTime.of(now, ZoneId.of("Etc/GMT-0"))
+    return ZoneOffset.getAvailableZoneIds().filterTo(mutableSetOf()) {
+        Duration.between(
+            greenwichTime,
+            ZonedDateTime.of(now, ZoneId.of(it))
+        ).abs().toMinutes() % 60 != 0L
+    }
 }
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    return generateSequence(LocalDate.of(year, 1, 31)) {
+        it.plusDays(1).with(TemporalAdjusters.lastDayOfMonth())
+    }
+        .takeWhile { it.year == year }
+        .map { it.dayOfWeek.name }
+        .toList()
 }
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    return generateSequence(LocalDate.of(year, 1, 1)) {
+        it.with(TemporalAdjusters.next(DayOfWeek.FRIDAY))
+    }
+        .takeWhile { it.year == year }
+        .count { it.dayOfMonth == 13 }
 }
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    return dateTime.format(DateTimeFormatter.ofPattern("dd LLL yyyy, HH:mm").withLocale(Locale.UK))
 }
 
 
