8e73bfc76e4a02b1c33a3b6dcbb921d23cfba6f0
anton <harbinger2113@gmail.com>
2023-10-29 20:25:07 +0300

    functional

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -1,10 +1,12 @@
 package ru.sber.functional
 
+import javax.sql.rowset.Predicate
+
 class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(predicate: (Student) -> Boolean) {
+        students = students.filter(predicate)
     }
 }
