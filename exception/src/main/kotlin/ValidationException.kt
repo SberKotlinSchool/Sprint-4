fa8e258c21155d9c048a04eea912a7b2b5d793b7
alexanastasyev <alex.anastasyev@mail.ru>
2023-11-01 21:16:23 +0300

    Exceptions task

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,25 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    FIRST_NAME_NULL_ERROR(101, "Не заполнено имя клиента"),
+    FIRST_NAME_NOT_CYRILLIC_ERROR(102, "В имени содержаться недопустимые символы"),
+    FIRST_NAME_TOO_LONG_ERROR(103, "Длина имени клиента превышает допустимую"),
+
+    LAST_NAME_NULL_ERROR(104, "Не заполнена фамилия клиента"),
+    LAST_NAME_NOT_CYRILLIC_ERROR(105, "В фамилии содержаться недопустимые символы"),
+    LAST_NAME_TOO_LONG_ERROR(106, "Длина фамилии клиента превышает допустимую"),
+
+    PHONE_NULL_ERROR(107, "Не заполнен номер телефона"),
+    PHONE_NOT_STARTS_FROM_7_OR_8_ERROR(108, "Номер телефона должен начинаться с 7 или 8"),
+    PHONE_NOT_CORRECT_LENGTH_ERROR(109, "Номер телефона должен состоять из 11 цифр"),
+    PHONE_NOT_NUMBERS_ERROR(110, "Номер телефона содержит недопустимые символы"),
+
+    EMAIL_NULL_ERROR(111, "Не заполнен email"),
+    EMAIL_WRONG_FORMAT_ERROR(113, "Email имеет некорректный формат"),
+    EMAIL_TOO_LONG_ERROR(114, "Длина email не должна превышать 32 символа"),
+
+    SNILS_NULL_ERROR(115, "Не заполнен СНИЛС"),
+    SNILS_NOT_NUMBERS_ERROR(116, "СНИЛС содержит недопустимые символы"),
+    SNILS_WRONG_LENGTH_ERROR(117, "СНИЛС должен состоять из 11 цифр"),
+    SNILS_CHECKSUM_ERROR(118, "Некорректный номер СНИЛС")
 }
--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -1,9 +1,8 @@
 import com.google.gson.Gson
 import org.junit.Test
-import org.junit.jupiter.api.assertThrows
-import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class ClientServiceTest {
 
@@ -18,20 +17,55 @@ class ClientServiceTest {
     }
 
     @Test
-    fun `fail save client - validation error`() {
+    fun `fail save client - phone incorrect`() {
         val client = getClientFromJson("/fail/user_with_bad_phone.json")
-        assertThrows<ValidationException>("Ожидаемая ошибка") {
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
+        val errors: Array<ErrorCode> = arrayOf(ErrorCode.PHONE_NOT_CORRECT_LENGTH_ERROR, ErrorCode.PHONE_NOT_STARTS_FROM_7_OR_8_ERROR)
+        assertTrue(errors contentEquals exception.errorCode)
     }
 
     @Test
-    fun `fail save client - validation errors`() {
+    fun `fail save client - many errors`() {
         val client = getClientFromJson("/fail/user_data_corrupted.json")
         val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+        val errors: Array<ErrorCode> = arrayOf(ErrorCode.FIRST_NAME_NULL_ERROR, ErrorCode.LAST_NAME_TOO_LONG_ERROR,
+                ErrorCode.PHONE_NOT_CORRECT_LENGTH_ERROR, ErrorCode.PHONE_NOT_STARTS_FROM_7_OR_8_ERROR,
+                ErrorCode.EMAIL_WRONG_FORMAT_ERROR, ErrorCode.SNILS_CHECKSUM_ERROR)
+        assertTrue(errors contentEquals exception.errorCode)
+    }
+
+    @Test
+    fun `fail save client - latin name`() {
+        val client = getClientFromJson("/fail/user_with_not_cyrillic_name.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        val errors: Array<ErrorCode> = arrayOf(ErrorCode.FIRST_NAME_NOT_CYRILLIC_ERROR, ErrorCode.LAST_NAME_NOT_CYRILLIC_ERROR)
+        assertTrue(errors contentEquals exception.errorCode)
+    }
+
+    @Test
+    fun `fail save client - too long email`() {
+        val client = getClientFromJson("/fail/user_data_too_long_email.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        val errors: Array<ErrorCode> = arrayOf(ErrorCode.EMAIL_TOO_LONG_ERROR)
+        assertTrue(errors contentEquals exception.errorCode)
+    }
+
+    @Test
+    fun `fail save client - corrupted snils`() {
+        val client = getClientFromJson("/fail/user_corrupted_snils.json")
+        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+            clientService.saveClient(client)
+        }
+        val errors: Array<ErrorCode> = arrayOf(ErrorCode.SNILS_WRONG_LENGTH_ERROR, ErrorCode.SNILS_NOT_NUMBERS_ERROR)
+        assertTrue(errors contentEquals exception.errorCode)
     }
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
