4976a177efdeebba91dc5e39baab1d79f4e08290
maximastashkin <maksim.astash@gmail.com>
2023-11-02 14:50:06 +0300

    Sprint-4 Task done

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -1,26 +1,39 @@
 package ru.sber.datetime
 
+import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.Month
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.Locale
+import java.util.TimeZone
 
 // 1.
-fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
-}
+fun getZonesWithNonDivisibleByHourOffset(): Set<String> = TimeZone.getAvailableIDs()
+    .map { TimeZone.getTimeZone(it) }
+    .filter { it.rawOffset % (60 * 60 * 1000) != 0 }
+    .map { it.displayName }
+    .toSet()
 
 // 2.
-fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+fun getLastInMonthDayWeekList(year: Int): List<String> = Month.values().map {
+    LocalDate.of(year, it, 1).with(TemporalAdjusters.lastDayOfMonth()).dayOfWeek.name
 }
 
 // 3.
-fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
-}
+fun getNumberOfFridayThirteensInYear(year: Int): Int = Month.values()
+    .map { LocalDate.of(year, it, 13) }
+    .count { it.dayOfWeek == DayOfWeek.FRIDAY }
 
 // 4.
-fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+fun getFormattedDateTime(dateTime: LocalDateTime): String =
+    DateTimeFormatter.ofPattern("dd MMM yyyy, HH:mm", Locale.US).format(dateTime)
+
+
+fun main() {
+    println(getZonesWithNonDivisibleByHourOffset())
+    println(getLastInMonthDayWeekList(2023))
+    println(getNumberOfFridayThirteensInYear(2023))
+    println(getFormattedDateTime(LocalDateTime.now()))
 }
-
-
-
--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -25,13 +25,47 @@ class ClientServiceTest {
         }
     }
 
+    // Данный тест закрывает 6 возможных ошибок. Позволю себе не делать 6 отдельных тестов на каждый вид
     @Test
     fun `fail save client - validation errors`() {
         val client = getClientFromJson("/fail/user_data_corrupted.json")
         val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+        assertEquals(6, exception.errorCode.size)
+        assertEquals(ErrorCode.NULL_VALUE, exception.errorCode[0])
+        assertEquals(ErrorCode.TOO_MUCH_LONG_STRING, exception.errorCode[1])
+        assertEquals(ErrorCode.INVALID_VALUE_LENGTH, exception.errorCode[2])
+        assertEquals(ErrorCode.INVALID_PHONE_CODE, exception.errorCode[3])
+        assertEquals(ErrorCode.INCORRECT_EMAIL_FORMAT, exception.errorCode[4])
+        assertEquals(ErrorCode.INVALID_SNILS_CONTROL_NUMBER, exception.errorCode[5])
+    }
+
+    @Test
+    fun `fail save client with invalid snils control number`() {
+        val client = getClientFromJson("/fail/user_invalid_snils_control_number.json")
+        val exception = assertFailsWith<ValidationException> {
+            clientService.saveClient(client)
+        }
+        assertEquals(ErrorCode.INVALID_SNILS_CONTROL_NUMBER, exception.errorCode.first())
+    }
+
+    @Test
+    fun `fail save client with invalid character`() {
+        val client = getClientFromJson("/fail/user_invalid_character.json")
+        val exception = assertFailsWith<ValidationException> {
+            clientService.saveClient(client)
+        }
+        assertEquals(ErrorCode.INVALID_CHARACTER, exception.errorCode.first())
+    }
+
+    @Test
+    fun `fail save client with empty phone`() {
+        val client = getClientFromJson("/fail/user_with_empty_phone.json")
+        val exception = assertFailsWith<ValidationException> {
+            clientService.saveClient(client)
+        }
+        assertEquals(ErrorCode.EMPTY_VALUE, exception.errorCode.first())
     }
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
